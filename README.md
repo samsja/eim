4f45727442843c0f50ba53889b64760f83f16841
sami jaghouar <sami.jaghouar@polynom.io>
2020-09-28 16:08:33 +0200

    migrate old repo library

---
+++ README.md
@@ -0,0 +1,35 @@
+# EIM ( Empirical Interpolation Method)
+
+
+* To install and uses the library
+
+```shell
+pip install dist/eim_samsja_faycal-0.0.1-py3-none-any.whl
+```
+
+* To build the packages from sources
+
+```shell
+python setup.py sdist bdist_wheel
+```
+
+
+
+
+## About the project
+
+This packages has been developed in the context of a research project at the [University Of Technology Of Compiègne (UTC)](https://www.utc.fr/) in France. 
+
+### Authors
+
+* **Sami Jaghouar** ( my repo)
+* **Faycal rekbi**
+
+### Contributor and Supervisor
+
+* **Florian De Vuyst** : head of LMAC ( [laboratory of Applied Mathematics of Compiègne](http://lmac.utc.fr/ )
+
+* **Anne-Virginie Salsac** : CNRS Director of Research at BMBI ( [laboraty of biomecanics et bioengineering](https://bmbi.utc.fr/))
+
+* **Claire Dupont**
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+
+with open("requirements.txt") as f:
+    packages = [line.strip() for line in f.readlines()]
+
+
+setuptools.setup(
+    name="eim-samsja-faycal", # Replace with your own username
+    version="0.0.1",
+    author="Sami Jaghouar And Faycal Rekbi",
+    author_email="sami.jaghouar@hotmail.fr faycal.rekbdi@etu.utc.fr",
+    description="implementation of Empirical Interpolation Methods (EIM) algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samsja/eim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = packages
+)
